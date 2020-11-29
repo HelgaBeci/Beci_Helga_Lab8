5d1644e5f5e3c09109f184933f091dff81d1ac3d
Helga Beci <helgaabeci@gmail.com>
2020-11-29 16:35:40 +0200

    Add project files.

---
+++ Data/Beci_Helga_Lab8Context.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Beci_Helga_Lab8.Models;
+
+namespace Beci_Helga_Lab8.Data
+{
+    public class Beci_Helga_Lab8Context : DbContext
+    {
+        public Beci_Helga_Lab8Context (DbContextOptions<Beci_Helga_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Beci_Helga_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Beci_Helga_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Beci_Helga_Lab8.Models.BookCategory> BookCategory { get; set; }
+
+        public DbSet<Beci_Helga_Lab8.Models.Category> Category { get; set; }
+    }
+}
